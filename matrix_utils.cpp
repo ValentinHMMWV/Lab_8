ed4562097917647eedc836db1798bd0772f854c9
ValentinHMMWV <0507669142dkw@gmail.com>
2026-03-23 20:52:40 +0200

    Create matrix_utils.cpp

---
+++ matrix_utils.cpp
@@ -0,0 +1,44 @@
+#include "matrix_utils.h"
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+const int MAX_M = 100;
+const int MAX_N = 100;
+
+void task2_matrix(const string filename) {
+    ifstream fin(filename);
+    ofstream fout("matrix_out.txt", ios::app);
+
+    int m, n;
+    fin >> m >> n;
+
+    double matrix[MAX_M][MAX_N];
+
+    // Зчитування
+    for (int i = 0; i < m; i++)
+        for (int j = 0; j < n; j++)
+            fin >> matrix[i][j];
+
+    // Swap квадрантів
+    int halfM = m / 2, halfN = n / 2;
+
+    for (int i = 0; i < halfM; i++)
+        for (int j = 0; j < halfN; j++)
+            swap(matrix[i][j], matrix[i + halfM][j + halfN]);
+
+    // Вивід
+    fout << "Matrix after swap:\n";
+
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++)
+            fout << matrix[i][j] << "\t";
+        fout << "\n";
+    }
+
+    cout << "Task 2 completed. Check matrix_out.txt\n";
+
+    fin.close();
+    fout.close();
+}
